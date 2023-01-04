138c9da9dc79f7406220f1cbdcbb6845e4109bc6
hayeon-lee <papepopepe@naver.com>
2023-01-04 22:10:19 +0900

    feat: add onChange func

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [change, setChange] = useState();
+  
   useEffect(()=>{
     //only run once
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -10,10 +12,17 @@ function App() {
     .then((json)=>setCoins(json)));
     setLoading(false);
   }, []);
+
+  const onChange = (event) => {
+    setChange(event.target.value);
+    console.log(change);
+  }
+
   return (
     <div>
         <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-        {loading ? <strong>Loading...</strong> : <select>
+        {loading ? <strong>Loading...</strong> : 
+        <select value = {change} onChange = {onChange}>
             {coins.map((coin)=><option>{coin.name} ({coin.symbol}): {coin.quotes.USD.price}</option>)}
         </select>}
         
