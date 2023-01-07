95ce34f21b675acd098cdb5ebb93d9044a4d68c9
Hayeon-Lee <papepopepe@naver.com>
2023-01-07 21:55:03 +0900

    add: props

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import Home from "./routes/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
-
+import Home from "./routes/Home";
 function App() {
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select value={change} onChange={onChange}>
-          {coins.map((coin, index) => (
-            <option key={index} value={coin.quotes.USD.price}>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price}
-            </option>
-          ))}
-        </select>
-      )}
-      <hr />
-      <div>
-        <strong>Dollar is {price}</strong>
-      </div>
-      <div>
-        <strong>Won is {price * 1250}</strong>
-      </div>
-      <hr />
-      <div>
-        <form>
-          <input type="Number" value={value} onChange={onInput} />
-          <button type="button" onClick={onClick}>
-            Convert
-          </button>
-        </form>
-        <strong>
-          한화로 {value}원 있을 때 {converted}개 살 수 있습니다.
-        </strong>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/abot-us">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
 export default App;
